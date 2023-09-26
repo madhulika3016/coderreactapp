a69e0f799726c891bcc5433e574dd7b655103482
Madhulika Dubey <57287028+madhulika3016@users.noreply.github.com>
2023-09-26 14:08:41 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import FooterComponent from './components/FooterComponent';
 import HeaderComponent from './components/HeaderComponent';
 import ListCoderComponent from './components/ListCoderComponent';
 import CoderComponent from './components/CoderComponent';
+import GetcoderByName1 from './components/GetCodorName';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
               <Route path = "/coders" element = { <ListCoderComponent /> }></Route>
               <Route path = "/add-coder" element = { <CoderComponent />} ></Route>
               <Route path = "/edit-coder/:id" element = { <CoderComponent />}></Route>
+               <Route path = "/getcoderbyname" element = { <GetcoderByName1 />}></Route>
             </Routes>
         </div>
         <FooterComponent />
